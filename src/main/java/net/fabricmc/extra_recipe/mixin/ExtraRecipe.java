317ea48157f2965cb6905f0cd320d48475765d1a
DiamondPigeon <112449001+RawDiamondMC@users.noreply.github.com>
2023-01-11 20:39:24 +0800

    1.0

--- src/main/java/net/fabricmc/extra_recipe/mixin/ExtraRecipe.java
+++ src/main/java/net/fabricmc/extra_recipe/mixin/ExtraRecipe.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.rawdiamondmc.extra_recipe.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.rawdiamondmc.extra_recipe.ExtraRecipe;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ExtraRecipeMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ExtraRecipe.LOGGER.info("This line is printed by Extra Recipe mod mixin!");
 	}
 }
