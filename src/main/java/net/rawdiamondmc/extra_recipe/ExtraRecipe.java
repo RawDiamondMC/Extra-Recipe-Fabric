dfaa0cb205a4986b22a94460fbe5567c9b180607
DiamondPigeon <112449001+RawDiamondMC@users.noreply.github.com>
2023-01-12 14:09:58 +0800

    Add record_blank

--- src/main/java/net/rawdiamondmc/extra_recipe/ExtraRecipe.java
+++ src/main/java/net/rawdiamondmc/extra_recipe/ExtraRecipe.java
@@ -1,6 +1,11 @@
 package net.rawdiamondmc.extra_recipe;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +14,8 @@ public class ExtraRecipe implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("extra_recipe");
+	// an instance of our new item
+	public static final Item record_blank = new Item(new FabricItemSettings());
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +24,6 @@ public class ExtraRecipe implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ITEM, new Identifier("extra_recipe", "record_blank"), record_blank);
 	}
 }
