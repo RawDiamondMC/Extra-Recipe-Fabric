0f1c74e83b16c735712c4c92bd9fa0a442b960d1
RawDiamondMC <112449001+RawDiamondMC@users.noreply.github.com>
2023-01-22 17:57:50 +0800

    Update ExtraRecipe.java

--- src/main/java/net/rawdiamondmc/extra_recipe/ExtraRecipe.java
+++ src/main/java/net/rawdiamondmc/extra_recipe/ExtraRecipe.java
@@ -12,9 +12,10 @@ public class ExtraRecipe implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("extra_recipe");
 	// an instance of our new item
-	public static final Item record_blank = new Item(new FabricItemSettings());
+	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
@@ -23,6 +24,6 @@ public class ExtraRecipe implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-		Registry.register(Registries.ITEM, new Identifier("extra_recipe", "record_blank"), record_blank);
+		Registry.register(Registry.ITEM, new Identifier("extra_recipe", "record_blank"), record_blank);
 	}
 }
